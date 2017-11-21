944d7683e0e1d85db0abaf7005e8ab7a93ecf625
Prathamesh <prathamesh2215@gmail.com>
2017-11-22 01:14:31 +0530

    Form Design dn, except goat income and expenditure and Total income bcoz of some doubts

--- src/pages/goat-details/goat-details.ts
+++ src/pages/goat-details/goat-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the GoatDetailsPage page.
@@ -15,11 +16,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GoatDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	goat_details: FormGroup;
+	submitAttempt: boolean = false;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GoatDetailsPage');
-  }
+	constructor(public navCtrl: NavController, 
+				public navParams: NavParams,
+				public toastCtrl: ToastController,
+				public formBuilder: FormBuilder) 
+	{
+		this.goat_details = formBuilder.group({
+			'f7_number_of_goats' : ['', Validators.required],
+			'f7_female_goat' : ['', Validators.required],
+			'f7_number_of_buck' : ['', Validators.required],
+			'f7_number_of_shegoat' : ['', Validators.required],
+			'f7_breed_name' : ['', Validators.required],
+			'f7_from_where_you_buy_goat' : ['', Validators.required],
+			'f7_type_of_goat_shed' : ['', Validators.required],
+			'f7_source_of_fodder' : ['', Validators.required],
+			'f7_own_fodder_lasts_in_num_of_days' : ['', Validators.required],
+			'f7_water_source' : ['', Validators.required],
+		});
+	}
+
+	ionViewDidLoad() 
+	{
+		console.log('ionViewDidLoad GoatDetailsPage');
+	}
+
+	showMessage(message, style: string, dur?: number)
+	{
+		const toast = this.toastCtrl.create({
+			message: message,
+			showCloseButton: true,
+			duration: dur || 5000,
+			closeButtonText: 'Ok',
+			cssClass: style,
+			dismissOnPageChange: true
+	    });
+
+	    toast.present();
+	}
+
+	save()
+	{
+		this.submitAttempt = true;
+		if (this.goat_details.valid) 
+		{
+			console.log(this.goat_details.value);
+		}
+		else
+		{
+			console.log('Validation error');
+			this.showMessage("Please fill valid data!", "danger", 100000);
+		}
+	}
 
 }
--- src/pages/goat-health-and-medical-details/goat-health-and-medical-details.ts
+++ src/pages/goat-health-and-medical-details/goat-health-and-medical-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the GoatHealthAndMedicalDetailsPage page.
@@ -15,11 +16,61 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GoatHealthAndMedicalDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	goat_health_and_medical: FormGroup;
+	submitAttempt: boolean = false;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GoatHealthAndMedicalDetailsPage');
-  }
+	constructor(public navCtrl: NavController, 
+				public navParams: NavParams,
+				public toastCtrl: ToastController,
+				public formBuilder: FormBuilder) 
+	{
+		this.goat_health_and_medical = formBuilder.group({
+			'f8_maintain_medical_records' : ['', Validators.required],
+			'f8_is_vaccinated' : ['', Validators.required],
+			'f8_vaccination_type' : ['', Validators.required],
+			'f8_vaccination_frequency' : ['', Validators.required],
+			'f8_vaccination_place' : ['', Validators.required],
+			'f8_number_of_vaccinated_goats' : ['', Validators.required],
+			'f8_infected_goats' : ['', Validators.required],
+			'f8_any_remedy_for_infection' : ['', Validators.required],
+			'f8_num_of_died_in_last_one_year' : ['', Validators.required],
+			'f8_reason_for_death' : ['', Validators.required],
+			'f8_num_of_goats_insured' : ['', Validators.required],
+			'f8_num_of_calving_cycles_for_past_two_years' : ['', Validators.required],
+			'f8_num_of_births_in_prev_year' : ['', Validators.required],
+		});
+	}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad GoatHealthAndMedicalDetailsPage');
+	}
+
+	showMessage(message, style: string, dur?: number)
+	{
+		const toast = this.toastCtrl.create({
+			message: message,
+			showCloseButton: true,
+			duration: dur || 5000,
+			closeButtonText: 'Ok',
+			cssClass: style,
+			dismissOnPageChange: true
+	    });
+
+	    toast.present();
+	}
+
+	save()
+	{
+		this.submitAttempt = true;
+		if (this.goat_health_and_medical.valid) 
+		{
+			console.log(this.goat_health_and_medical.value);
+		}
+		else
+		{
+			console.log('Validation error');
+			this.showMessage("Please fill valid data!", "danger", 100000);
+		}
+	}
 
 }
